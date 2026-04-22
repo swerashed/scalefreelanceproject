56ad706f0881ebac2fc242fe43cc23ee0b4c73f7
Rashed <rashed@notionhive.com>
2026-04-22 20:31:19 +0600

    refactor: update pricing banner layout, typography, and button icon styling

--- components/pricing-banner-inner.php
+++ components/pricing-banner-inner.php
@@ -2,23 +2,31 @@
 $data = getData();
 ?>
 <!-- Section Banner -->
-<section class="generic-section-banner pricing-banner">
+<section class="pricing-banner">
     <div class="container">
         <div class="section-header text-center">
-            <h2>
-                <?php echo $data['title']; ?>
-            </h2>
-            <p>
-                <?php echo $data['description']; ?>
-            </p>
+            <div>
+                <h2>
+                    <?php echo $data['title']; ?>
+                </h2>
+            </div>
+            <div>
+                <p>
+                    <?php echo $data['description']; ?>
+                </p>
+            </div>
 
             <?php if (!empty($data['btn_title'])): ?>
                 <div class="banner-btn-wrapper">
                     <a href="<?php echo !empty($data['btn_link']) ? $data['btn_link'] : '#'; ?>" class="banner-link">
                         <?php echo $data['btn_title']; ?>
                         <span class="icon-circle">
-                            <svg width="12" height="12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M18 9.0001C18 9.0001 13.5811 15 12 15C10.4188 15 6 9.00005 6 9.00005" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <rect x="0.5" y="0.5" width="17" height="17" rx="8.5" stroke="#989FB3"
+                                    stroke-opacity="0.4" />
+                                <path
+                                    d="M5.25921 8.9L5.79921 8.37L8.73921 11.29V5.5H9.55921V11.27L12.4892 8.36L13.0292 8.9L9.36921 12.5H8.91921L5.25921 8.9Z"
+                                    fill="#989FB3" />
                             </svg>
                         </span>
                     </a>
